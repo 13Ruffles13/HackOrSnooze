0ae41f35cdf0f57c23bce2a305e4677023a570dd
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-06-02 18:21:04 -0700

    UpdatedComments

--- js/stories.js
+++ js/stories.js
@@ -3,48 +3,53 @@
 // This is the global list of the stories, an instance of StoryList
 let storyList;
 
-/** Get and show stories when site first loads. */
-
+/**
+ * Get and show stories when the site first loads.
+ */
 async function getAndShowStoriesOnStart() {
+  // Retrieve the list of stories
   storyList = await StoryList.getStories();
+
+  // Remove the loading message from the page
   $loadingMessageElement.remove();
 
+  // Display the stories on the page
   putStoriesOnPage();
 }
 
 /**
- * A render method to render HTML for an individual Story instance
- * - story: an instance of Story
+ * Generates HTML markup for an individual Story instance.
+ * - story: An instance of Story.
+ * - showDeleteBtn: A boolean indicating whether to show the delete button (default: false).
  *
- * Returns the markup for the story.
+ * Returns the HTML markup for the story.
  */
-
 function generateStoryMarkup(story, showDeleteBtn = false) {
-  // console.debug("generateStoryMarkup", story);
-
   const hostName = story.getHostName();
-
   const showStar = Boolean(currentUser);
 
   return $(`
-  <li id="${story.storyId}">
-    <div>
-    ${showDeleteBtn ? getDeleteBtnHTML() : ""}
-    ${showStar ? getStarHTML(story, currentUser) : ""}
-    <a href="${story.url}" target="a_blank" class="story-link">
-      ${story.title}
-    </a>
-    <small class="story-hostname">(${hostName})</small>
-    <div class="story-author">by ${story.author}</div>
-    <div class="story-user">posted by ${story.username}</div>
-    </div>
-  </li>
-`);
+    <li id="${story.storyId}">
+      <div>
+        ${showDeleteBtn ? getDeleteBtnHTML() : ""}
+        ${showStar ? getStarHTML(story, currentUser) : ""}
+        <a href="${story.url}" target="_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <div class="story-author">by ${story.author}</div>
+        <div class="story-user">posted by ${story.username}</div>
+      </div>
+    </li>
+  `);
 }
 
-// . Make a delete btn HTML for story
+/**
+ * Generates the HTML for a delete button for a story.
+ *
+ * Returns the HTML markup for the delete button.
+ */
 function getDeleteBtnHTML() {
-  console.log("getDeleteBtnHTML");
   return `
     <span class="trash-can">
       <i class="fas fa-trash-alt"></i>
@@ -52,74 +57,94 @@ function getDeleteBtnHTML() {
   `;
 }
 
-// . Make a fav / not-fav star for story
+/**
+ * Generates the HTML for a favorite/not-favorite star for a story.
+ * - story: An instance of Story.
+ * - user: The current user.
+ *
+ * Returns the HTML markup for the star.
+ */
 function getStarHTML(story, user) {
-  console.debug("getStarHTML");
   const isFav = user.isFavorite(story);
   const starType = isFav ? "fas" : "far";
+
   return `
     <span class="star">
-      <i class="${starType} fa-star"></i> 
-    </span> 
+      <i class="${starType} fa-star"></i>
+    </span>
   `;
 }
-/** Gets list of stories from server, generates their HTML, and puts on page. */
 
+/**
+ * Retrieves a list of stories from the server, generates their HTML markup, and puts them on the page.
+ */
 function putStoriesOnPage() {
   console.debug("putStoriesOnPage");
 
+  // Empty the stories list element
   $allStoriesListElement.empty();
 
-  // loop through all of our stories and generate HTML for them
+  // Loop through all stories and generate HTML for each of them
   for (let story of storyList.stories) {
     const $story = generateStoryMarkup(story);
     $allStoriesListElement.append($story);
   }
 
+  // Show the stories list element
   $allStoriesListElement.show();
 }
 
 /* ----------------- Functions added ---------------- */
 
-// . Delete a story - handler
+/**
+ * Deletes a story.
+ * - evt: The event object.
+ */
 async function deleteStory(evt) {
   console.debug("deleteStory");
 
+  // Find the closest <li> element and retrieve the story ID
   const $closestLi = $(evt.target).closest("li");
   const storyId = $closestLi.attr("id");
 
+  // Delete the story from the story list
   await storyList.deleteStory(currentUser, storyId);
 
-  //Refresh story list
+  // Refresh the user's story list on the UI
   await putUserStoriesOnUI();
 }
 
 $ownStoriesElement.on("click", deleteStory);
 
-// Submit a new story -- Handler will be called
+// Submit a new story - Handler will be called
 async function submitNewStory(evt) {
   console.debug("submitNewStory");
   evt.preventDefault();
 
-  //Get data from the form
+  // Get data from the form
   const title = $("#create-title").val();
   const url = $("#create-url").val();
   const author = $("#create-author").val();
   const username = currentUser.username;
   const storyData = { title, url, author, username };
 
+  // Add the story to the story list
   const story = await storyList.addStory(currentUser, storyData);
 
+  // Generate HTML markup for the new story and prepend it to the stories list
   const $story = generateStoryMarkup(story);
   $allStoriesListElement.prepend($story);
 
-  //Hide the form and reset functionality
+  // Hide the form and reset its functionality
   $submitFormElement.slideUp("slow");
   $submitFormElement.trigger("reset");
 }
+
 $submitFormElement.on("submit", submitNewStory);
 
-// Current user own stories
+/**
+ * Displays the current user's own stories on the UI.
+ */
 function putUserStoriesOnUI() {
   console.debug("putUserStoriesOnUI");
   $ownStoriesElement.empty();
@@ -152,7 +177,7 @@ function putFavoriteStoriesOnUI() {
   $favoriteStoriesElement.show();
 }
 
-// Toggle fav/un-fav a story
+// Toggle favorite/unfavorite a story
 async function toggleFavoriteStories(evt) {
   console.debug("toggleFavoriteStories");
   const $tgt = $(evt.target);
@@ -161,9 +186,11 @@ async function toggleFavoriteStories(evt) {
   const story = storyList.stories.find((s) => s.storyId === storyId);
 
   if ($tgt.hasClass("fas")) {
+    // Remove story from favorites
     await currentUser.removeFavorite(story);
     $tgt.closest("i").toggleClass("fas far");
   } else {
+    // Add story to favorites
     await currentUser.addFavorite(story);
     $tgt.closest("i").toggleClass("fas far");
   }
