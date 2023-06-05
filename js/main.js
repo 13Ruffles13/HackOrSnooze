3a939574ccfabecac18e4f0acec900f7ac5a866b
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-06-04 21:13:20 -0700

    updatedComments

--- js/main.js
+++ js/main.js
@@ -1,31 +1,49 @@
 "use strict";
 
+/**
+ * jQuery Elements
+ */
+
+// Body element of the page
 const $bodyElement = $("body");
 
+// Loading message element
 const $loadingMessageElement = $("#stories-loading-msg");
+
+// Elements that contain the lists of stories
 const $allStoriesListElement = $("#all-stories-list");
 const $favoriteStoriesElement = $("#favorited-stories");
 const $ownStoriesElement = $("#my-stories");
+
+// Element that contains the stories
 const $storiesContainerElement = $("#stories-container");
 
+// List of all story elements
 const $storiesLists = $(".stories-list");
 
+// Element that contains the login and signup forms
 const $loginAndSignupElement = $("#login-signup-forms");
+
+// Login form element
 const $loginFormElement = $("#login-form");
+
+// Signup form element
 const $signupFormElement = $("#signup-form");
+
+// Submit form element
 const $submitFormElement = $("#submit-form");
 
+// Navigation elements
 const $navSubmitStoryElement = $("#nav-submit-story");
 const $navLoginElement = $("#nav-login");
 const $navUserProfileElement = $("#nav-user-profile");
 const $navLogOutElement = $("#nav-logout");
 
+// User profile element
 const $userProfileElement = $("#user-profile");
 
-/**
- * Hides all page components to make it easier for individual components to show themselves.
- * After calling this function, individual components can re-show just what they want.
- */
+// Hides all page components to make it easier for individual components to show themselves.
+// After calling this function, individual components can re-show just what they want.
 function hidePageComponents() {
   const components = [
     $storiesLists,
@@ -37,9 +55,7 @@ function hidePageComponents() {
   components.forEach((c) => c.hide());
 }
 
-/**
- * Overall function to kick off the app.
- */
+// Overall function to kick off the app.
 async function start() {
   console.debug("start");
 
@@ -54,6 +70,10 @@ async function start() {
     updateUIOnUserLogin();
   }
 }
+
+/**
+ * Event Listeners and DOM Ready
+ */
 
 // Once the DOM is entirely loaded, begin the app
 console.warn(
